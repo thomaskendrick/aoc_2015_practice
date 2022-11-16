959cabbc8d02d866ac7e073c0f5a70ed1fcbae16
Thomas <tom@tkendrick.com>
2022-11-16 20:23:56 +0000

    day 15 part 2

--- a2015_15/src/lib.rs
+++ a2015_15/src/lib.rs
@@ -34,7 +34,7 @@ impl FromStr for Ingredient {
     }
 }
 
-pub fn part_a(input: &str) -> i32 {
+pub fn part_a_b(input: &str, cap_calories: bool) -> i32 {
     let ingredients: Vec<Ingredient> = input.lines().map(|s| s.parse().unwrap()).collect();
     if ingredients.len() != 4 {
         panic!()
@@ -46,34 +46,40 @@ pub fn part_a(input: &str) -> i32 {
         for i1 in 0..=100 - i0 {
             for i2 in 0..=100 - i0 - i1 {
                 for i3 in 0..=100 - i0 - i1 - i2 {
-                    let capacity =
-                        0.max(
-                            ingredients[0].capacity * i0 + 
-                            ingredients[1].capacity * i1 + 
-                            ingredients[2].capacity * i2 + 
-                            ingredients[3].capacity * i3
-                        );
-                    let durability =
-                        0.max(
-                            ingredients[0].durability * i0 + 
-                            ingredients[1].durability * i1 + 
-                            ingredients[2].durability * i2 + 
-                            ingredients[3].durability * i3
-                        );
-                    let flavour =
-                        0.max(
-                            ingredients[0].flavour * i0 + 
-                            ingredients[1].flavour * i1 + 
-                            ingredients[2].flavour * i2 + 
-                            ingredients[3].flavour * i3
-                        );
-                    let texture =
-                        0.max(
-                            ingredients[0].texture * i0 + 
-                            ingredients[1].texture * i1 + 
-                            ingredients[2].texture * i2 + 
-                            ingredients[3].texture * i3
-                        );
+                    let calories = 0.max(
+                        ingredients[0].calories * i0
+                            + ingredients[1].calories * i1
+                            + ingredients[2].calories * i2
+                            + ingredients[3].calories * i3,
+                    );
+                    if cap_calories && calories != 500 {
+                        continue;
+                    }
+
+                    let capacity = 0.max(
+                        ingredients[0].capacity * i0
+                            + ingredients[1].capacity * i1
+                            + ingredients[2].capacity * i2
+                            + ingredients[3].capacity * i3,
+                    );
+                    let durability = 0.max(
+                        ingredients[0].durability * i0
+                            + ingredients[1].durability * i1
+                            + ingredients[2].durability * i2
+                            + ingredients[3].durability * i3,
+                    );
+                    let flavour = 0.max(
+                        ingredients[0].flavour * i0
+                            + ingredients[1].flavour * i1
+                            + ingredients[2].flavour * i2
+                            + ingredients[3].flavour * i3,
+                    );
+                    let texture = 0.max(
+                        ingredients[0].texture * i0
+                            + ingredients[1].texture * i1
+                            + ingredients[2].texture * i2
+                            + ingredients[3].texture * i3,
+                    );
                     best_score = best_score.max(capacity * durability * flavour * texture);
                 }
             }
@@ -88,6 +94,10 @@ mod tests {
     const INPUT: &str = include_str!("input.txt");
     #[test]
     fn part_a() {
-        assert_eq!(super::part_a(INPUT), 0);
+        assert_eq!(super::part_a_b(INPUT, false), 13882464);
+    }
+    #[test]
+    fn part_b() {
+        assert_eq!(super::part_a_b(INPUT, true), 11171160);
     }
 }
